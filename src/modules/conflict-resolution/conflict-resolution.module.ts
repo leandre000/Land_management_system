144d57ac843b8785b587b54e8e51e0bfaec25fd5
Shema-Leandre <Iamshemaleandre@gmail.com>
2025-07-20 21:23:49 +0200

    added the notification module

--- src/modules/conflict-resolution/conflict-resolution.module.ts
+++ src/modules/conflict-resolution/conflict-resolution.module.ts
@@ -5,12 +5,14 @@ import { ConflictResolutionController } from './conflict-resolution.controller';
 import { LandDispute } from './entities/land-dispute.entity';
 import { LandRegistrationModule } from '../land-registration/land-registration.module';
 import { UsersModule } from '../users/users.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([LandDispute]),
     LandRegistrationModule,
     UsersModule,
+    NotificationsModule
   ],
   controllers: [ConflictResolutionController],
   providers: [ConflictResolutionService],
